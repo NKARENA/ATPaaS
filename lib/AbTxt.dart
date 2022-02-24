144a403555c2a1dfc0bef2d7a3acec67416cc9e6
Nidesh Kaarthik <rs.nideshkaarthik@gmail.com>
2022-02-24 22:13:21 +0530

    Second file added

---
+++ lib/AbTxt.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AbTxt extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "ATPaaS",
+      style: TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+}
